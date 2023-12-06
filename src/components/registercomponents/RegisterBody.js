399953a1a44c3182b274eba032151473602d920e
lalitha12-web <lalitha.ujjaneni@gmail.com>
2023-12-06 12:45:59 +0530

    updated on 12/6

--- src/components/registercomponents/RegisterBody.js
+++ src/components/registercomponents/RegisterBody.js
@@ -18,12 +18,12 @@ function RegisterBody() {
   const [registrationSuccess, setRegistrationSuccess] = useState(false); // New state variable
   const [registrationInProgress, setRegistrationInProgress] = useState(false);
   const [otpSendingInProgress, setOTPSendingInProgress] = useState(false); // New state variable for sending OTP
-  const [otpVerifyingInProgress, setOTPVerifyingInProgress] = useState(false); // New state variable for verifying OTP
- 
+  const [otpVerifyingInProgress, setOTPVerifyingInProgress] = useState(false); // New state variable for verifying OTP 
+
   const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
- 
+
   const handleSendOTP = async () => {
     if (!isFormValid()) {
       return; // Do not proceed with registration if form is invalid
@@ -38,12 +38,12 @@ function RegisterBody() {
       setOTPSendingInProgress(false); // Clear sending OTP in progress in case of error
     }
   };
- 
+
   const handleSendOTP1 = async () => {
     if (!isFormValid()) {
       return; // Do not proceed with registration if form is invalid
     }
- 
+
     try {
       setOTPSendingInProgress(true); // Set sending OTP in progress
      const response= await axios.post(`${apiUrl}/recuriters/registration-send-otp`, { email });
@@ -52,20 +52,20 @@ function RegisterBody() {
       if(response.body==='Email is already  registered.'){
         window.alert('Email is already  registered.');
        }
-   
+    
     } catch (error) {
       console.error('Error sending OTP:', error);
       setOTPSendingInProgress(false); // Clear sending OTP in progress in case of error
     }
   };
- 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
- 
+
     if (!isFormValid()) {
       return; // Do not proceed with registration if form is invalid
     }
- 
+
     try {
       setRegistrationInProgress(true);
       // Send the form data to the backend server using Axios POST request
@@ -75,14 +75,14 @@ function RegisterBody() {
         mobilenumber,
         password,
       });
- 
+
       // Clear any previous error messages if the registration is successful
       // If OTP is sent, render OTPVerification component
-     
+      
       setErrorMessage('');
       setRegistrationSuccess(true);
- 
-     
+
+      
       // Handle the response from the server (e.g., show a success message)
       console.log('Registration successful', response.data);
       // Show a window alert for successful registration
@@ -91,7 +91,7 @@ function RegisterBody() {
       setMobilenumber('');
       setPassword('');
       setRegistrationInProgress(false);
- 
+
       // Use the navigate function to navigate to the home screen
       if (otpSent && otpVerified) {
         navigate('/login', { state: { registrationSuccess: true } });
@@ -103,68 +103,55 @@ function RegisterBody() {
       window.alert('Registration failed! user with this email already exist');
       console.error('Registration failed', error);
     }
- 
+
 };
- 
+
 const isEmailValid = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
- 
+
   const isPasswordValid = (password) => {
     // Password must be at least 6 characters long
     if (password.length < 6) {
       return false;
     }
- 
+  
     // Password must contain at least one uppercase letter
     if (!/[A-Z]/.test(password)) {
       return false;
     }
- 
+  
     // Password must contain at least one special character (non-alphanumeric)
     if (!/[^A-Za-z0-9]/.test(password)) {
       return false;
     }
- 
+  
     // Password cannot contain spaces
     if (/\s/.test(password)) {
       return false;
     }
- 
+  
     return true;
   };
- 
+  
   const isMobileNumberValid = (mobilenumber) => {
     // Mobile number must contain only numeric digits
     if (!/^\d+$/.test(mobilenumber)) {
       return false;
     }
- 
+
     // Mobile number must have a specific length (e.g., 10 digits)
     if (mobilenumber.length !== 10) {
       return false;
     }
- 
+
     const firstDigit = mobilenumber.charAt(0);
   if (!['6', '7', '8', '9'].includes(firstDigit)) {
     return false;
   }
     return true;
   };
-  const isCompanyValid = (companyname) => {
-  // Company name must not be empty
-  if (!companyname) {
-    return false;
-  }
- 
-  // Company name must be at least 6 characters long
-  if (companyname.length < 6) {
-    return false;
-  }
- 
-  return true;
-};
   const isFormValid = () => {
     if (!isEmailValid(email)) {
       setErrorMessage('Please enter a valid email address.');
@@ -175,40 +162,36 @@ const isEmailValid = (email) => {
       return false;
     }
     if (!isPasswordValid(password)) {
-      setErrorMessage('Password must be at least 6 characters long and should have one capital letter and one small letter and no spaces are allowed.');
+      setErrorMessage('Password must be at least 6 characters long and should have one cpital letter and one small letter and no spaces are allowed.');
       return false;
     }
-    if (!isCompanyValid(companyname)) {
-    setErrorMessage('Please enter a valid company name (at least 6 characters).');
-    return false;
-  }
- 
+
     return true;
-   
+    
   };
- 
+
   const handleSubmit1 = async (e) => {
     e.preventDefault();
- 
+
    
     try {
       setRegistrationInProgress(true);
       const response = await axios.post(`${apiUrl}/recuriters/saverecruiters`, {
        
-        companyname,
+        companyname, 
         mobilenumber,
         email,
         password,
       });
- 
+
       setErrorMessage('');
       setRegistrationSuccess(true);
- 
+
       console.log('Registration successful', response.data);
-     
+      
       setRegistrationInProgress(false);
       if (otpSent && otpVerified) {
-         
+          
           navigate('/recruiterlogin', { state: { registrationSuccess: true } });
       }
     } catch (error) {
@@ -217,7 +200,7 @@ const isEmailValid = (email) => {
       console.error('Registration failed', error);
     }
   };
- 
+
   return (
     <div>
     <section className="bg-f5">
@@ -236,7 +219,7 @@ const isEmailValid = (email) => {
         </div>
       </div>
     </section>
- 
+
     <section className="account-section">
       <div className="tf-container">
         <div className="row">
@@ -311,16 +294,15 @@ const isEmailValid = (email) => {
       disabled={otpSent || registrationInProgress || otpSendingInProgress}
     >
       {otpSendingInProgress ? (
-       
+        
         <div className="spinner"></div>
        
       ) : (
-      'Resend OTP'
+        <div></div>
       )}
     </button>
   </div>
 ) : (
- 
   <div>
     {otpVerified ? (
       <div style={{ color: 'green' }}>
@@ -347,8 +329,7 @@ const isEmailValid = (email) => {
     )}
   </div>
 )}
- 
- 
+
 {otpVerified && (
   <button type="submit">
     {registrationInProgress ? (
@@ -361,8 +342,8 @@ const isEmailValid = (email) => {
     )}
   </button>
 )}
- 
- 
+
+
                 </form>
               </div>
             </div>
@@ -422,7 +403,7 @@ const isEmailValid = (email) => {
       disabled={otpSent || registrationInProgress || otpSendingInProgress}
     >
       {otpSendingInProgress ? (
-       
+        
         <div className="spinner"></div>
        
       ) : (
@@ -438,7 +419,7 @@ const isEmailValid = (email) => {
       </div>
     ) : (
       <div>
-       
+        
         <button
           type="button"
           onClick={handleSendOTP1}
@@ -457,7 +438,7 @@ const isEmailValid = (email) => {
     )}
   </div>
 )}
- 
+
 {otpVerified && (
   <button type="submit">
     {registrationInProgress ? (
@@ -470,8 +451,8 @@ const isEmailValid = (email) => {
     )}
   </button>
 )}
- 
- 
+
+
                 </form>
               </div>
             </div>
@@ -480,8 +461,8 @@ const isEmailValid = (email) => {
       </div>
     </section>
   </div>
-   
+    
   );
 }
- 
+
 export default RegisterBody;
