ae66cdd1255ad065e4dfa476c21d00bc8911072d
srilathamanam <srilatha.manam@gmail.com>
2023-12-28 12:15:34 +0530

    Update Logout.js

--- src/components/common/Logout.js
+++ src/components/common/Logout.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ApplicantAPIService, { apiUrl } from '../../services/ApplicantAPIService';
-
+ 
 const Logout = () => {
+  const [isLoggedOut, setIsLoggedOut] = useState(false);
   const navigate = useNavigate();
-
+ 
   useEffect(() => {
     const signOutUser = async () => {
       try {
@@ -12,29 +13,45 @@ const Logout = () => {
           method: 'POST',
           credentials: 'include',
         });
-
+ 
+       
+ 
         if (response.status === 204) {
-          // Redirect to the home page after logging out
-          navigate('/');
-          console.log('Redirecting to the home page...');
-
-          // Use setTimeout to ensure that localStorage is cleared after the redirect
-          setTimeout(() => {
-            localStorage.removeItem('jwtToken');
-            localStorage.removeItem('userType');
-          }, 0);
+          setIsLoggedOut(true);
+ 
+         
         } else {
+          // Handle the error, e.g., display an error message
           console.error('Sign-out request failed.');
+          setIsLoggedOut(true); // If there's an error, still consider the user as logged out
         }
       } catch (error) {
+        // Handle network or other errors
         console.error('Error signing out:', error);
+        setIsLoggedOut(true); // If there's an error, still consider the user as logged out
       }
     };
-
+ 
     signOutUser();
-  }, [navigate]);
-
-  return <p>Logging out...</p>;
+  }, []); // Empty dependency array means this effect runs once after the initial render
+ 
+  return (
+    <div>
+      {isLoggedOut ? (
+        <div>
+        {(() => {
+         
+          localStorage.removeItem('jwtToken');
+          localStorage.removeItem('user');
+          navigate('/');
+         
+        })()}
+      </div>
+      ) : (
+        <p>Logging out...</p>
+      )}
+    </div>
+  );
 };
-
+ 
 export default Logout;
