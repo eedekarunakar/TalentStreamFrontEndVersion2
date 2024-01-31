046b98edb466844ff73111ad915fd8d7a9d8250d
eedekarunakar <karunakar.eede@bitlabs.in>
2024-01-31 19:00:27 +0530

    data added 31 jan 2024

--- src/components/logincomponents/LoginBody.js
+++ src/components/logincomponents/LoginBody.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import { useUserContext } from '../common/UserProvider';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { apiUrl } from '../../services/ApplicantAPIService';
+import { useGoogleLogin } from '@react-oauth/google'; // Import GoogleLoginButton
+import jwt_decode from "jwt-decode";
  
 function LoginBody({ handleLogin }) {
   const [candidateEmail, setCandidateEmail] = useState('');
@@ -21,7 +23,177 @@ function LoginBody({ handleLogin }) {
   const [candidatePasswordError, setCandidatePasswordError] = useState('');
   const [recruiterEmailError, setRecruiterEmailError] = useState('');
   const [recruiterPasswordError, setRecruiterPasswordError] = useState('');
-//  const [candidateLoginInProgress, setCandidateLoginInProgress] = useState(false);
+ const [candidateLoginInProgress, setCandidateLoginInProgress] = useState(false);
+// const login = useGoogleLogin({
+//   // clientId: "435586738795-9tuq57be4e92djg8d8ol1sn1h6a9mm6c.apps.googleusercontent.com", // Pass the clientId here
+//   onSuccess: async (response) => {
+//     try{
+//       console.log('first API');
+//      const res = await axios.get(
+//       "https://www.googleapis.com/oauth2/v3/userinfo",
+//       {
+//         headers:{
+//         Authorization:`Bearer ${response.access_token}`,
+//         },
+//       }
+//      );
+//      console.log(res);
+//      const email1 = res.data.email;
+//      console.log('Second API');
+//     let loginEndpoint = `${apiUrl}/applicant/applicantLogin`;
+//       const response1 = await axios.post(loginEndpoint, {
+//         email: email1,
+       
+//       });
+ 
+//  console.log(res);
+//  if (response.status === 200) {
+//         setErrorMessage('');
+//         const userData = response.data;
+//         console.log('this is response ', userData);
+//         console.log('this is token ', userData.data.jwt);
+//         localStorage.setItem('jwtToken', userData.data.jwt);
+ 
+//         let userType1;
+ 
+//         if (userData.message.includes('ROLE_JOBAPPLICANT')) {
+//           userType1 = 'jobseeker';
+//         } else if (userData.message.includes('ROLE_JOBRECRUITER')) {
+//           userType1 = 'employer';
+//         } else {
+//           userType1 = 'unknown';
+//         }
+//         console.log('this userType ', userType1);
+//         localStorage.setItem('userType', userType1);
+ 
+//         setErrorMessage('');
+//         handleLogin();
+ 
+//         setUser(userData);
+//         setUserType(userData.userType);
+//         console.log('Login successful', userData);
+ 
+//  navigate('/applicanthome');
+//  }
+//  catch(err){
+//  console.log(err);
+//  }  
+//  },
+// });
+ 
+const login = useGoogleLogin({
+  // clientId: "435586738795-9tuq57be4e92djg8d8ol1sn1h6a9mm6c.apps.googleusercontent.com", // Pass the clientId here
+  onSuccess: async (response) => {
+    try {
+      console.log('First API');
+      const res = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        {
+          headers: {
+            Authorization: `Bearer ${response.access_token}`,
+          },
+        }
+      );
+      console.log(res);
+      const email1 = res.data.email;
+      console.log('Second API');
+      let loginEndpoint = `${apiUrl}/applicant/applicantLogin`;
+      const response1 = await axios.post(loginEndpoint, {
+        email: email1,
+      });
+     
+      console.log(response1);
+      if (response1.status === 200) {
+        setErrorMessage('');
+        const userData = response1.data; // Change `response` to `response1`
+        console.log('This is response: ', userData);
+        console.log('This is token: ', userData.data.jwt);
+        localStorage.setItem('jwtToken', userData.data.jwt);
+ 
+        let userType1;
+        if (userData.message.includes('ROLE_JOBAPPLICANT')) {
+          userType1 = 'jobseeker';
+        } else if (userData.message.includes('ROLE_JOBRECRUITER')) {
+          userType1 = 'employer';
+        } else {
+          userType1 = 'unknown';
+        }
+        console.log('This userType: ', userType1);
+        localStorage.setItem('userType', userType1);
+ 
+        setErrorMessage('');
+        handleLogin();
+ 
+        setUser(userData);
+        setUserType(userType1); // Change `userData.userType` to `userType1`
+        console.log('Login successful', userData);
+ 
+        navigate('/applicanthome');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+});
+ 
+ 
+// const onSuccessGoogleSignIn = async (response) => {
+//   try {
+//     const userInfoResponse = await axios.get(
+//       "https://www.googleapis.com/oauth2/v3/userinfo",
+//       {
+//         headers: {
+//           Authorization: `Bearer ${response.access_token}`,
+//         },
+//       }
+//     );
+ 
+//     const { email } = userInfoResponse.data;
+ 
+//     // Send email to backend to generate JWT token
+//     const loginEndpoint = `${apiUrl}/applicant/applicantLogin`;
+//     const loginResponse = await axios.post(loginEndpoint, { email });
+ 
+//     // Store JWT token in local storage
+//     localStorage.setItem('jwtToken', loginResponse.data.jwt);
+ 
+//     // Determine user type based on response
+//     let userType;
+//     if (loginResponse.data.message.includes('ROLE_JOBAPPLICANT')) {
+//       userType = 'jobseeker';
+//     } else if (loginResponse.data.message.includes('ROLE_JOBRECRUITER')) {
+//       userType = 'employer';
+//     } else {
+//       userType = 'unknown';
+//     }
+ 
+//     // Store user type in local storage
+//     localStorage.setItem('userType', userType);
+ 
+//     // Redirect to appropriate home page
+//     if (userType === 'jobseeker') {
+//       navigate('/applicanthome');
+//     } else if (userType === 'employer') {
+//       navigate('/recruiterhome');
+//     } else {
+//       // Handle unknown user type
+//     }
+//   } catch (error) {
+//     console.error("Error:", error);
+//     // Handle error
+//   }
+// };
+ 
+// const onFailureGoogleSignIn = (error) => {
+//   console.error("Error:", error);
+//   // Handle error
+// };
+ 
+// const googleLogin = useGoogleLogin({
+//   onSuccess: onSuccessGoogleSignIn,
+//   onFailure: onFailureGoogleSignIn
+// });
+ 
  
  
   const handleTogglePassword = () => {
@@ -265,7 +437,12 @@ const handleRecruiterSubmit = async (e) => {
                       <div className="sign-up">
                         Not registered yet? <a href="/register">Sign Up</a>
                       </div>
+       
                     </form>
+                    <br></br>
+                    {/* <button onClick={() => login()}>Sign in with Google 🚀</button> */}
+                    <a href="#" class="btn-social" onClick={() => login()}> <img src="images/review/google.png" alt="images" /> Continue with Google</a>
+                    {/* <button onClick={googleLogin}>Sign in with Google</button> */}
                   </div>
  
                   <div className="inner" style={{ display: activeTab === 'Employer' ? 'block' : 'none' }}>
@@ -313,6 +490,8 @@ const handleRecruiterSubmit = async (e) => {
                       <div className="sign-up">
                         Not registered yet? <a href="/register">Sign Up</a>
                       </div>
+                     
+       
                     </form>
                   </div>
                 </div>
